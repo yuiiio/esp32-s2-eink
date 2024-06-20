494758d0b2cfb53a4df85f25822dd3b06ac19df0
yuiiio <atbjyk@protonmail.com>
2024-06-21 00:02:35 +0900

    update to tiff script

--- convert-to-eink-image.sh
+++ convert-to-eink-image.sh
@@ -1,2 +1,3 @@
 # thanks for https://middleriver.chagasi.com/electronics/epd_photo/
-convert $1 -resize 1448x1072 -gravity center -extent 1448x1072 -colorspace gray -remap hald:4 -depth 4 -compress none output.tif
+convert $1 -alpha off -rotate $2 -resize 1448x1072 -gravity center -extent 1448x1072 -colorspace gray -remap hald:4 -depth 4 -compress none output.tif
+convert $1 -alpha off -rotate $2 -resize 1448x1072 -gravity center -extent 1448x1072 -colorspace gray -remap hald:4 -depth 4 -compress none output.png # for preview
