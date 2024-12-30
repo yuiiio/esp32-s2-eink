5b580024ac545f2281433ee1d182836943006cbc
yuiiio <atbjyk@protonmail.com>
2024-12-30 18:37:31 +0900

    convert-files

--- convert-files.sh
+++ convert-files.sh
@@ -1,4 +1,9 @@
+
 NUM=0
+if [ -n $3 ]; then
+    NUM=$3
+fi
+
 while read file; do
     convert "$1"/"${file}" -alpha off -rotate $2 -resize 1448x1072 -gravity center -extent 1448x1072 -colorspace gray -remap hald:4 -depth 4 -compress none $(printf "%03d" ${NUM}).tif
     echo $((NUM++))
