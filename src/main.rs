b9af28637d6df3eee34ca47eb92b60ceaabc276d
yuiiio <atbjyk@protonmail.com>
2025-03-29 22:54:43 +0900

    still finding best repaint behavour

--- src/main.rs
+++ src/main.rs
@@ -938,10 +938,12 @@ fn main() -> ! {
 
     let mut volume_manager = VolumeManager::new(sdcard, FakeTimesource {});
 
+    /*
     let mut img_buf_1: Vec<u8, _> = Vec::with_capacity_in(FOUR_BPP_BUF_SIZE, &esp_alloc::HEAP);
     for _i in 0..FOUR_BPP_BUF_SIZE {
         img_buf_1.push(0u8);
     }
+    */
 
     // too big for dram? so use psram(2M)
     let mut img_buf_2: Vec<u8, _> = Vec::with_capacity_in(FOUR_BPP_BUF_SIZE, &esp_alloc::HEAP);
@@ -1098,7 +1100,7 @@ fn main() -> ! {
         cur_dir_files_len
     };
 
-    let pre_buf = &mut img_buf_1;
+    //let pre_buf = &mut img_buf_1;
     let mut next_buf = &mut img_buf_2;
 
     let mut cur_page: u16 = if last_opend_page_num > cur_dir_files_len {
@@ -1412,10 +1414,12 @@ fn main() -> ! {
         write!(&mut file_name, "{0: >03}.tif", cur_page).unwrap();
         match open_4bpp_image(&mut cur_child_dir, &mut next_buf, &file_name) {
             Ok(_) => {
-                eink_display.write_4bpp_partialy_reverse_image(&pre_buf);
+                //eink_display.write_4bpp_reverse_image(&pre_buf);
+                //eink_display.write_4bpp_partialy_reverse_image(&pre_buf);
                 //eink_display.write_all_black_white();
-                //eink_display.write_all_white();
-                //eink_display.write_all_black();
+                eink_display.write_all_white();
+                eink_display.write_all_white();
+                eink_display.write_all_black();
                 //eink_display.write_4bpp_reverse_image(&next_buf);
                 eink_display.write_4bpp_image(&next_buf);
                 flash
@@ -1431,6 +1435,6 @@ fn main() -> ! {
                 eink_display.write_all_black();
             }
         };
-        core::mem::swap(pre_buf, next_buf);
+        //core::mem::swap(pre_buf, next_buf);
     }
 }
